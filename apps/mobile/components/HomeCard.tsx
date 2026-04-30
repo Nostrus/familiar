70e5fc9023f1f67ecfd5fcd5e19e6e0c6e00f0fc
Csaba Illés <thecsabailles@proton.me>
2026-04-30 20:40:18 +0200

    fix: fix shadow clipping

--- apps/mobile/components/HomeCard.tsx
+++ apps/mobile/components/HomeCard.tsx
@@ -10,10 +10,7 @@ export type HomeCardProps = {
 
 export function HomeCard({ image, title, location, price }: HomeCardProps) {
   return (
-    <View
-      className="w-[220px] rounded-2xl bg-white mr-6 ml-1 p-3 my-4"
-      style={{ boxShadow: '0 4px 16px rgba(0,0,0,0.12)' }}
-    >
+    <View className="w-[220px] rounded-2xl bg-white mr-6 p-3 my-4 shadow-sm border-gray-100 border">
       <Image source={{ uri: image }} className="w-full h-[120px] rounded-xl mb-2" />
       <Text className="font-bold text-base mb-0.5">{title}</Text>
       <Text className="text-sm text-gray-600 mb-0.5">{location}</Text>
